b489641200036d5a71e5ebc1dfbeed4ac6e85c25
GW_MC <ping.chan78@gmail.com>
2022-01-10 16:57:27 +0800

    fixed image position out of bound

--- public/javascripts/changeContent.js
+++ public/javascripts/changeContent.js
@@ -2,6 +2,7 @@ contentnameToPath = {
 	facebookRC: '/html/cssRecreation/facebookRC.html',
 	instgramRC: '/html/cssRecreation/instgramRC.html',
 	twitterRC: '/html/cssRecreation/twitterRC.html',
+	login: '/html/login.html',
 };
 function changeContent(contentName) {
 	path = contentnameToPath[contentName];
